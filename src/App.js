b259bfa24be5a521e1b4998a6e29a50dffbc5a0f
Prahlad Kumar <prahladk09@gmail.com>
2022-05-10 18:06:32 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { LoginContext } from "./components/Context/LoginContext";
+import Home from "./components/Home";
+import Login from "./components/Login";
+
+function App() {
+  const [isLoggedIn, updateLogin] = useState(false);
+  return (
+    <div className="App">
+      <LoginContext.Provider value={{ isLoggedIn, updateLogin }}>
+        {isLoggedIn ? <Home /> : <Login />}
+      </LoginContext.Provider>
+    </div>
+  );
+}
+
+export default App;
